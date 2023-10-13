925aa043d182a5fa8a37720211c839d42fa3f4ef
mehmet <mehmetsaidcanli@gmail.com>
2023-10-13 14:47:00 +0300

    Initial Commit

---
+++ VideoView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoView
+//
+//  Created by Mehmet Said Canlı on 13.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
